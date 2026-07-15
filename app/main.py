a497596e0f708827f47a24fbf179754b19664b6e
muhammadsohaib796 <sohaibshakeel796@gmail.com>
2026-07-15 22:37:11 +0500

    main.py Modified

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from app.routes.doctor import router as doctor_router
+from app.routes.patient import router as patient_router
 
 app = FastAPI()
 
 app.include_router(doctor_router)
+app.include_router(patient_router)
 
 @app.get("/")
 def read_root():
